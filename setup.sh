b9d1837d47f1e9c33e467b36e9308e9eb781d337
Munetaka Mizutani <mu.mizutani@gmail.com>
2017-06-17 14:22:08 +0900

    rm unnecessary target

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
 
- DOT_FILES=( .zsh .zshrc .zshrc.custom .zshrc.alias .zshrc.linux .zshrc.osx .ctags .emacs.el .gdbinit .gemrc .gitconfig .gitignore .inputrc .irbrc .sbtconfig .screenrc .vimrc .vim .vrapperrc import.scala .tmux.conf .dir_colors .rdebugrc .pryrc)
+ DOT_FILES=( .zshrc .zshrc.alias .zshrc.osx .gitconfig .gitignore .vimrc .tmux.conf )
 
  for file in ${DOT_FILES[@]}
  do
@@ -8,4 +8,4 @@
  done
 
  # install oh-my-zsh
- [! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+ # [! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
